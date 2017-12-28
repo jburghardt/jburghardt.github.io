d1b90e0823617aec658debb025875537ff5147c1
Johannes Burghardt <johannes.burghardt@seitenbau.com>
2017-12-28 13:48:33 +0100

    update stuff

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div> 'hiidfsdfi' </div>,
+  <div> 'under construction' </div>,
   document.getElementById('container')
 );
